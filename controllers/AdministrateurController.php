8e99635151e619549bf88d5eba2e6ece0e3ac4ea
oumaima <oumaimaabouelhaitam@gmail.com>
2022-05-20 20:29:01 +0100

    update file AdministrateurController

--- controllers/AdministrateurController.php
+++ controllers/AdministrateurController.php
@@ -16,51 +16,80 @@ class AdministrateurController{
       $p->job=$_POST['Job'];
       $p->setAdresse($_POST['adres']);
       $p->phone=$_POST['Phone'];
-      if($p->Ajouter()){
-        header('location:parent');
-      }
+      if($p->Ajouter()) header('location:parent');
       }
     }
     public function deleteparent(){
       if(isset($_POST['deletparent'])){
          $p = new Parents();
          $p->setMatricule($_POST['Matricule']);
-         if($p->delete()){
-          header('location:parent');
-         }
+         if($p->delete()) header('location:parent');
         } 
     }
 
-   function updateparent(){
+    public function updateparent(){
     if(isset($_POST['bntupdate'])){
-    $p = new Parents();
-    $p->setMatricule($_POST['matricule']);
+       $p = new Parents();
+        $p->setMatricule($_POST['matricule']);
         $p->setNom($_POST['nom']);
         $p->setGenre($_POST['genre']);
         $p->job=$_POST['Job'];
         $p->setAdresse($_POST['adres']);
         $p->phone=$_POST['Phone'];
-        if($p->Update()){
-          header('location:parent');
-        }
+        if($p->Update())header('location:parent');
       }
    }
-    // public function getAllstudent(){
-    //   include('models/étudiant.php');
-    //   $student=new Etudiant();
-    //   return $student->Afficher();
-    // }
+  //  ------------------------------------------------------------------------
+    public function getAllstudent(){
+   
+    }
+    public function addstudent(){
+    
+    }
+    public function deletestudent(){
+    
+    }
 
-    // public function getAllprofesseur(){
-    //   include('models/professeur.php');
-    //   $professeur=new professeur();
-    //   return $professeur->Afficher();
-    // }
+    public function updatestudent(){
 
-    // public function getAlladmin(){
-    //   include('models/Administrateur.php');
-    //   $professeur=new Administrateur();
-    //   return $professeur->Afficher();
-    // }
+    }
+      //  ------------------------------------------------------------------------
+
+
+    public function getAllprofesseur(){
+     
+    }
+    public function addprofesseur(){
+    
+    }
+    public function deleteprofesseurt(){
+    
+    }
+
+    public function updateprofesseur(){
+
+    }
+      //  ------------------------------------------------------------------------
+
+
+    public function getAlladmin(){
+     
+    }
+    public function addadmin(){
+    
+    }
+    public function deleteadmin(){
+    
+    }
+
+    public function updateadmin(){
+
+    }
+      //  ------------------------------------------------------------------------
+     
+      public function signin(){
+
+      }
+
 }
 ?>
