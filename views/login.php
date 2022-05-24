f3cbbfed46af1b736b0e676135d1532b94771598
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2022-05-24 09:06:44 +0100

    signin

--- views/login.php
+++ views/login.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <style>
         *{
             padding: 0;
@@ -23,13 +22,11 @@
             margin-top: 20%;
             align-items: center;
         }
-
         img{
             border-top-left-radius:30px;
             border-bottom-left-radius:30px;
             width: 100%;
         }
-
         .btn1{
             border:none;
             outline: none;
@@ -38,51 +35,55 @@
             background-color: #9573D4;
             color: white;
             border-radius: 4px;
-            font-weight: bold;
-        }
+            font-weight: bold;}
 
             .btn1:hover{
                 background: white;
                 border:1px solid;
                 color:black;
             }
-            </style>
+
+    </style>
+
 </head>
-<body>
-<section class="Form my-4 mx-5">
+  <body>
+  <section class="Form my-4 mx-5">
         <div class="container">
             <div class="row no-gutters">
                 <div class="col-lg-5">
-                    <img src="./views/img/login.png" alt="">
+                    <img src="../views/img/login.png" alt="">
                 </div>
                 <div class="col-lg-7 px--5 pt-5">
                     <h1 style="color:orange;">Signin</h1>
                     <h4>Signin into your account</h4>
-                    <form action="operation" method="POST">
+                    <form>
                         <div class="form-row>">
                             <div class="col-lg-7">
-                                <input type="text" placeholder=" Email address" name="Nom" class="form-control my-3 p-4">
+                                <input type="text" placeholder=" Email address" class="form-control my-3 p-4">
                             </div>
                         </div>
                         <div class="form-row>">
                             <div class="col-lg-7">
-                                <input type="password" placeholder="***********" name="password" class="form-control my-3 p-4">
+                                <input type="password" placeholder="***********" class="form-control my-3 p-4">
                             </div>
                         </div>
                         <div class="form-row>">
                             <div class="col-lg-7">
-                                <button type="submit" class="btn1 mt-3 mb-3" name="login" >Login</button>
+                                <button type="submit" class="btn1 mt-3 mb-3">Login</button>
                             </div>
                         </div>
                         <a href="" style="text-decoration: none; color:#28881D">Forgot password?</a>
                     </form>
-                </div>
+                    </div>
             </div>
             </div>
 
 </section>
-   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
 </html>
